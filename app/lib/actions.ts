57d8b70f64060d5d4f86417bb3bd71ceb86fe849
Anton Menkveld <anton@parallelsoftware.com.au>
2025-06-22 17:45:17 +0200

    88%

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -6,28 +6,50 @@ import { redirect } from 'next/navigation'
 
 const FormSchema = z.object({
   id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(),
-  status: z.enum(['paid', 'pending']),
+  customerId: z.string({
+    invalid_type_error: 'Please select a customer'
+  }),
+  amount: z.coerce.number().gt(0, 'Amount must be greater than $0'),
+  status: z.enum(['paid', 'pending'], {
+    invalid_type_error: 'Please select a invoice status'
+  }),
   date: z.string()
 })
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true })
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' })
 
-export async function createInvoice(formData: FormData) {
-  const { customerId, amount, status } = CreateInvoice.parse({
+export type State = {
+  errors?: {
+    customerId?: string[]
+    amount?: string[]
+    status?: string[]
+  }
+  message?: string | null
+}
+
+export async function createInvoice(prevState: State, formData: FormData) {
+  const validatedFields = CreateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status')
   })
-  const amountInCents = amount * 100
+
+  // If form validation fails, return errors early. Otherwise, continue.
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Create Invoice.'
+    }
+  }
+
+  const amountInCents = validatedFields.data.amount * 100
   const date = new Date().toISOString().split('T')[0]
 
   try {
     await sql`
         INSERT INTO invoices (customer_id, amount, status, date)
-        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+        VALUES (${validatedFields.data.customerId}, ${amountInCents}, ${validatedFields.data.status}, ${date})
       `
   } catch (error) {
     console.error('Database Error:', error)
@@ -40,15 +62,21 @@ export async function createInvoice(formData: FormData) {
 // Use Zod to update the expected types
 const UpdateInvoice = FormSchema.omit({ id: true, date: true })
 
-// ...
-
-export async function updateInvoice(id: string, formData: FormData) {
-  const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(id: string, prevState: State, formData: FormData) {
+  const validatedFields = UpdateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status')
   })
 
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Update Invoice.'
+    }
+  }
+
+  const { customerId, amount, status } = validatedFields.data
   const amountInCents = amount * 100
 
   try {
